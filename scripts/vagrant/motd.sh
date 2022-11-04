11e516260ac26851f23b35785a2fe5152f8216e1
CarlosSanabriaM <uo250707@uniovi.es>
2022-11-04 11:38:52 +0100

    refactor: `motd.sh`

--- scripts/vagrant/motd.sh
+++ scripts/vagrant/motd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Constants
-COWSAY_FILES_ARRAY=("default" "three-eyes" "bud-frogs" "sheep" "moofasa" "moose" "tux")
+COWFILES_ARRAY=("default" "three-eyes" "bud-frogs" "sheep" "moofasa" "moose" "tux")
 MOTD_FIGLET_HEADER="Vagrant VM"
 MOTD_TMUX_COMMANDS="Attach to the tmux session using the following command:
 $ tmux attach -t python"
@@ -15,17 +15,16 @@ apt-get install -y figlet
 apt-get install -y lolcat
 apt-get install -y cowsay
 
-# Get array size
-cowsayFilesArraySize=${#COWSAY_FILES_ARRAY[@]}
-
-# Get last array index
-cowsayFilesArrayLastIndex=$((cowsayFilesArraySize-1))
-
-# Generate random value in the range [0, cowsayFilesArrayLastIndex]
-cowsayFilesArrayRandomIndex=$(shuf -i 0-$cowsayFilesArrayLastIndex -n 1)
-
-# Get value from the array, using the random index
-cowsayFile=${COWSAY_FILES_ARRAY[$cowsayFilesArrayRandomIndex]}
+# Randomly choose a cowfile to be used with `cowsay`
+#  Get array size
+cowfilesArraySize=${#COWFILES_ARRAY[@]}
+#  Get last array index
+cowfilesArrayLastIndex=$((cowfilesArraySize-1))
+#  Generate random value in the range [0, cowfilesArrayLastIndex]
+cowfilesArrayRandomIndex=$(shuf -i 0-$cowfilesArrayLastIndex -n 1)
+#  Get value from the array, using the random index
+cowfile=${COWFILES_ARRAY[$cowfilesArrayRandomIndex]}
+echo "$cowfile"
 
 # Create a message of the day that will be displayed on login, storing it in /etc/motd
 #  Welcome banner created with `figlet` and `lolcat`
@@ -33,8 +32,8 @@ figlet -t "$MOTD_FIGLET_HEADER" | # print header using `figlet`
   lolcat --spread 1.5 --force > /etc/motd # add rainbow coloring
 #  Tmux commands for attaching to sessions created with `cowsay`
 echo "$MOTD_TMUX_COMMANDS" |
-  cowsay -n -f "$cowsayFile" | # print message using `cowsay` and the randomly selected cowfile
+  cowsay -n -f "$cowfile" | # print message using `cowsay` and the randomly selected cowfile
   perl -pe 's/\$ ([^>|\/\\]+)/\e[1;35m\$ \e[1;36m$1\e[0m/' >> /etc/motd # use regex to add coloring to tmux command
 
 # Log message
-echo "Message Of The Day (MOTD) updated with cowsay file: $cowsayFile"
+echo "Message Of The Day (MOTD) updated with cowfile: $cowfile"
