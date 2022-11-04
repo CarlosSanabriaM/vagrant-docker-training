0d3409812dacfc44e625b96d1339392d15a22329
CarlosSanabriaM <uo250707@uniovi.es>
2022-11-04 11:00:54 +0100

    feat: add color to MOTD tmux commands

--- scripts/vagrant/motd.sh
+++ scripts/vagrant/motd.sh
@@ -3,7 +3,7 @@
 # Constants
 COWSAY_FILES_ARRAY=("default" "three-eyes" "bud-frogs" "sheep" "moofasa" "moose" "tux")
 MOTD_FIGLET_HEADER="Vagrant VM"
-MOTD="Attach to the tmux session using the following command:
+MOTD_TMUX_COMMANDS="Attach to the tmux session using the following command:
 $ tmux attach -t python"
 
 # Suppress `dpkg-preconfigure` warning related to stdin for `apt-get install`
@@ -28,8 +28,13 @@ cowsayFilesArrayRandomIndex=$(shuf -i 0-$cowsayFilesArrayLastIndex -n 1)
 cowsayFile=${COWSAY_FILES_ARRAY[$cowsayFilesArrayRandomIndex]}
 
 # Create a message of the day that will be displayed on login, storing it in /etc/motd
-figlet -t "$MOTD_FIGLET_HEADER" | lolcat --spread 1.5 --force > /etc/motd
-printf "%s" "$MOTD" | /usr/games/cowsay -n -f "$cowsayFile" >> /etc/motd
+#  Welcome banner created with `figlet` and `lolcat`
+figlet -t "$MOTD_FIGLET_HEADER" | # print header using `figlet`
+  lolcat --spread 1.5 --force > /etc/motd # add rainbow coloring
+#  Tmux commands for attaching to sessions created with `cowsay`
+echo "$MOTD_TMUX_COMMANDS" |
+  cowsay -n -f "$cowsayFile" | # print message using `cowsay` and the randomly selected cowfile
+  perl -pe 's/\$ ([^>|\/\\]+)/\e[1;35m\$ \e[1;36m$1\e[0m/' >> /etc/motd # use regex to add coloring to tmux command
 
 # Log message
 echo "Message Of The Day (MOTD) updated with cowsay file: $cowsayFile"
